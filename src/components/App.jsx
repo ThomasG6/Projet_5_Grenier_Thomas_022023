6b26794c424c062b41ef2a7b0dfd880276cc25fd
Thomas Grenier <grenier.tjm@gmail.com>
2023-02-26 20:58:14 +0100

    remove banner layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import Home from "../pages/Home";
 import Housing from "../pages/Housing";
 import About from "../pages/About";
 import Error404 from "../pages/Error404";
-import BannerLayout from "../layouts/BannerLayout";
 import DefaultLayout from "../layouts/DefaultLayout";
 
 import styles from "./App.module.scss";
@@ -13,12 +12,9 @@ function App() {
   return (
     <div className={styles.app}>
       <Routes>
-        <Route element={<BannerLayout />}>
+        <Route element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-        </Route>
-
-        <Route element={<DefaultLayout />}>
           <Route path="/housing/:id" element={<Housing />} />
           <Route path="*" element={<Error404 />} />
         </Route>
