a929ae1c9dc20d7fb7e236be280997d7c1925682
Thomas Grenier <grenier.tjm@gmail.com>
2023-02-27 04:52:50 +0100

    add collapse in about page

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,26 @@
+import styles from "./Collapse.module.scss";
+import { useState } from "react";
+import vector from "../../images/vector.png";
+
+function Collapse({ title, description }) {
+  const [opener, setOpener] = useState(false);
+
+  return (
+    <div className={styles.collapse} id={`clollapse--${title}`}>
+      <div className={styles.collapse__header}>
+        <div className={styles.collapse__header__title}>{title}</div>
+        <div
+          className={`collapse__header__vector ${opener}`}
+          onClick={() => setOpener(!opener)}
+        >
+          <img src={vector} alt="ouvrir la liste" />
+        </div>
+      </div>
+      {opener && (
+        <div className={styles.collapse__description}>{description}</div>
+      )}
+    </div>
+  );
+}
+
+export default Collapse;
