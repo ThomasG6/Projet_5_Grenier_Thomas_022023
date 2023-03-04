26fabb980f99612b5e2402f7e87e10332e76d844
Thomas Grenier <grenier.tjm@gmail.com>
2023-03-04 21:09:12 +0100

    add collapse in housing page

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -2,16 +2,29 @@ import styles from "./Housing.module.scss";
 import { useParams, Navigate } from "react-router-dom";
 import Housinglist from "../data.json";
 import Carrousel from "../components/Carrousel";
+import Collapse from "../components/Collapse";
 
 function Housing() {
   const id = useParams();
   const housingDetails = Housinglist.find((logement) => logement.id === id.id);
 
+  const equipmentsHousing = housingDetails?.equipments.map(
+    (equipment, index) => {
+      return <li key={index}>{equipment}</li>;
+    }
+  );
   return (
     <>
       {housingDetails ? (
-        <div className={styles.details}>
+        <div className={styles.housing}>
           <Carrousel images={housingDetails?.pictures} />
+          <div className={styles.housing__details}>
+            <Collapse
+              title="Description"
+              description={housingDetails?.description}
+            />
+            <Collapse title="Equipements" description={equipmentsHousing} />
+          </div>
         </div>
       ) : (
         <Navigate replace to="/Error404" />
