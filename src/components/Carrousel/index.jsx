79feff56e0be1ebc2412782977c6f33389a3b1e3
Thomas Grenier <grenier.tjm@gmail.com>
2023-03-04 20:20:03 +0100

    add carrousel in housing page

---
+++ src/components/Carrousel/index.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import styles from "./Carrousel.module.scss";
+import arrow from "../../images/arrow.png";
+
+function Carrousel({ images }) {
+  let [imageDisplay, imageChange] = useState(0);
+  let imageQuantity = images.length;
+
+  const previousImg = () => {
+    if (imageDisplay === 0) {
+      imageChange(imageQuantity - 1);
+    } else {
+      imageChange(imageDisplay - 1);
+    }
+    return imageChange;
+  };
+
+  const imgFollowing = () => {
+    if (imageDisplay === imageQuantity - 1) {
+      imageChange((imageQuantity = 0));
+    } else {
+      imageChange(imageDisplay + 1);
+    }
+    return imageChange;
+  };
+  console.log({ images });
+  return (
+    <div className={styles.carrousel}>
+      {imageQuantity > 1 && (
+        <img
+          className={`${styles.arrow} ${styles["arrow__right"]}`}
+          src={arrow}
+          alt="précedent"
+          onClick={previousImg}
+        />
+      )}
+      {images.map((image, index) => {
+        return (
+          <img
+            key={index}
+            className={
+              index === imageDisplay
+                ? `${styles["carrousel__img"]} ${styles.active}`
+                : styles.carrousel__img
+            }
+            src={image}
+            alt="Logement"
+          />
+        );
+      })}
+      {imageQuantity > 1 && (
+        <img
+          className={`${styles.arrow} ${styles["arrow__left"]}`}
+          src={arrow}
+          alt="suivant"
+          onClick={imgFollowing}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Carrousel;
--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,5 +1,23 @@
+import styles from "./Housing.module.scss";
+import { useParams, Navigate } from "react-router-dom";
+import Housinglist from "../data.json";
+import Carrousel from "../components/Carrousel";
+
 function Housing() {
-  return <>Fiche Logement</>;
+  const id = useParams();
+  const housingDetails = Housinglist.find((logement) => logement.id === id.id);
+
+  return (
+    <>
+      {housingDetails ? (
+        <div className={styles.details}>
+          <Carrousel images={housingDetails?.pictures} />
+        </div>
+      ) : (
+        <Navigate replace to="/Error404" />
+      )}
+    </>
+  );
 }
 
 export default Housing;
